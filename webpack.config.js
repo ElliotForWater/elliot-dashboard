9ddba7d50e53ed5414d60982cff1a11ec1d728fe
designbygio <designbygio@pm.me>
2021-06-09 15:17:48 +0200

    add icons, webpack for css

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,23 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 
 const buildTarget = process.env.BUILD_TARGET || 'web'
 const isProduction = process.env.NODE_ENV === 'production'
+const isWeb = buildTarget === 'web'
 const version = require('./package.json').version
 
 const config = {
-  entry: './src/index.tsx',
+  entry: {
+    main: ['./src/index.tsx', './src/styles/main.css'],
+  },
   target: 'web',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].[chunkhash:12].js',
+    filename: isWeb ? '[name].[chunkhash:12].js' : '[name].js',
   },
   mode: isProduction ? 'production' : 'development',
   resolve: {
@@ -26,13 +30,16 @@ const config = {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader',
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
       {
         test: /\.(gif|jpe?g|png)$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: '[name].[chunkhash:12].[ext]',
+          name: isWeb ? '[name].[chunkhash:12].[ext]' : '[name].js',
         },
       },
       {
@@ -57,6 +64,9 @@ const config = {
     new HtmlWebpackPlugin({
       template: `./target/${buildTarget}/index.html`,
     }),
+    new MiniCssExtractPlugin({
+      filename: isWeb ? '[name].[hash:12].css' : '[name].css',
+    }),
     new webpack.EnvironmentPlugin({
       BUILD_TARGET: 'web',
       VERSION: version,
@@ -65,7 +75,7 @@ const config = {
   devServer: {
     overlay: true,
   },
-  devtool: buildTarget === 'web' ? 'source-map' : false,
+  devtool: isWeb ? 'source-map' : false,
   stats: {
     warnings: false,
   },
