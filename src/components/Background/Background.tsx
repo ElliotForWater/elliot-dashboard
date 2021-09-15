d50d77a97bbc1f289c2ae4173a7f77259b144afe
designbygio <designbygio@pm.me>
2021-09-15 14:20:28 +0200

    stuff

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -16,39 +16,18 @@ const Background = function ({ photo, children }) {
     <div className={styles.container}>
       {/* Images w size token from: https://html.com/attributes/img-srcset */}
       {photo?.urls.raw && (
-        <>
-          {/* <img
+        <div className={styles.imageContainer}>
+          <img
             className={styles.img}
             srcSet={`
-              ${photo.urls.raw}&dpr=1 1000w,
-              ${photo.urls.raw}&dpr=2 2013w,
-              ${photo.urls.raw}&dpr=3 3019w,
-              ${photo.urls.raw}&dpr=4 4025w`
-            }
+              ${photo.urls.regular}&dpr=1 1000w,
+              ${photo.urls.regular}&dpr=2 2013w,
+              ${photo.urls.regular}&dpr=3 3019w,
+              ${photo.urls.regular}&dpr=4 4025w`}
             src={photo.urls.regular}
-            alt={photo.alt_description}
-          /> */}
-          <div
-            style={{
-              backgroundColor: '#505a602b',
-              transition: 'opacity 200ms ease-in-out',
-              top: '0',
-              left: '0',
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-            }}
-          >
-            <div
-              style={{
-                backgroundImage: `url(${photo.urls.raw})`,
-                backgroundSize: 'cover',
-                height: '100%',
-              }}
-            >
-              {content}
-            </div>
-          </div>
+          />
+
+          {content}
 
           <footer className={styles.footer}>
             <div>
@@ -57,7 +36,7 @@ const Background = function ({ photo, children }) {
             </div>
             <div>{photo.location.title}</div>
           </footer>
-        </>
+        </div>
       )}
     </div>
   )
